f4df594979057cfa94df9b40718be66ed3e622fe
Sudeep Ranjan Sahoo <sudeepranjan@iitbhilai.ac.in>
2023-07-25 07:03:07 +0530

    20

--- 20.valid-parentheses.cpp
+++ 20.valid-parentheses.cpp
@@ -5,11 +5,28 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    bool isValid(string s) {
-        
+    bool isValid(string s)
+    {
+        stack<char> st;
+        for (auto i : s)
+        {
+            if (i == '(' || i == '{' || i == '[')
+            {
+                st.push(i);
+            }
+            else
+            {
+                if (st.empty() || (i == ')' && st.top() != '(') || (i == '}' && st.top() != '{') || (i == ']' && st.top() != '['))
+                {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
     }
 };
 // @lc code=end
-
