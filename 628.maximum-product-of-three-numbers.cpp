39e837ba9c01e800aef4cc415d7878e2abfa3d2f
Sudeep Ranjan Sahoo <sudeepranjan@iitbhilai.ac.in>
2023-07-25 08:11:49 +0530

    628

---
+++ 628.maximum-product-of-three-numbers.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=628 lang=cpp
+ *
+ * [628] Maximum Product of Three Numbers
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+    int n = nums.size();
+    sort(nums.begin(),nums.end());
+        return max(nums[n-1]*nums[n-2]*nums[n-3],nums[0]*nums[1]*nums[n-1]);
+    }
+};
+// @lc code=end
+
